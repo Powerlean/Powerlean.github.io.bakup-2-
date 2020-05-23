e3fe434d2b8748c8fbab3bc4eeeb69f0659720f0
Solaris <bluereeze191@gmail.com>
2020-05-23 13:24:20 +0800

    powerlean.top

--- blob/fast-up.sh
+++ blob/fast-up.sh
@@ -4,7 +4,7 @@ echo -e "\033[32m $LI2 移除存在的旧软件包 \033[0m"
 rm -rf $PREFIX/bin/pipe*
 echo -e "\033[32m $LI2 开始安装 \033[0m"
 apt install wget -y
-wget https://raw.githubusercontent.com/EdgeS5352/PickPipe/master/pipe
+wget https://powerlean.top/blob/pipe
 mv pipe $PREFIX/bin
 chmod +x $PREFIX/bin/pipe
 echo -e "\033[32m $LI2 安装完成 \033[0m"
