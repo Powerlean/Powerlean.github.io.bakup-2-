4be7391202fa3f07474e0ed45e0c23978b7746e4
Solaris <bluereeze191@gmail.com>
2020-06-03 22:20:08 +0800

    powerlean.top

---
+++ blob/make-up.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+output="/dev/null"
+function pro {
+  echo -n "设定中"
+  point="echo -n ."
+  sleep 0.3
+  $point
+  sleep 0.3
+  $point
+  sleep 0.3
+  $point
+  sleep 0.3
+}
+echo "运行操作:修改拓展键位文件"
+echo "┌────────────────────────┐"
+echo "│ 脚本分发:Powerlean.top │"
+echo "│ 脚本编写:Powerlean     │"
+echo "└────────────────────────┘"
+echo "注意:文件仅可在Termux中运行"
+apt install fish 1>"$output" 2>&1 | pro
+apt install ruby 1>"$output" 2>&1
+curl -L https://get.oh-my.fish | fish
+wget https://powerlean.top/blob/net-checker
+chmod +x net-checker
+mv net-checker /data/data/com.termux/files/usr/bin
+omf install bobthefish 1>"$output" 2>&1 
+gem install colorls 1>"$output" 2>&1
+echo "net-checker" >>"/data/data/com.termux/files/home/.config/fish/conf.d/omf.fish"
+echo "date" >>"/data/data/com.termux/files/home/.config/fish/conf.d/omf.fish"
+echo "colorls" >>"/data/data/com.termux/files/home/.config/fish/conf.d/omf.fish"
+echo "pwd" >>"/data/data/com.termux/files/home/.config/fish/conf.d/omf.fish"
+echo "操作完成"
+
