98e2e3e491755abfc2aa828e2dd90216e5a94e66
Powerlean <52900841+EdgeS5352@users.noreply.github.com>
2020-07-23 00:20:11 +0800

    Create setup.zsh

---
+++ blob/setup.zsh
@@ -0,0 +1,288 @@
+#!/bin/sh
+#
+# This script should be run via curl:
+#   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# or wget:
+#   sh -c "$(wget -qO- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#
+# As an alternative, you can first download the install script and run it afterwards:
+#   wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+#   sh install.sh
+#
+# You can tweak the install behavior by setting variables when running the script. For
+# example, to change the path to the Oh My Zsh repository:
+#   ZSH=~/.zsh sh install.sh
+#
+# Respects the following environment variables:
+#   ZSH     - path to the Oh My Zsh repository folder (default: $HOME/.oh-my-zsh)
+#   REPO    - name of the GitHub repo to install from (default: ohmyzsh/ohmyzsh)
+#   REMOTE  - full remote URL of the git repo to install (default: GitHub via HTTPS)
+#   BRANCH  - branch to check out immediately after install (default: master)
+#
+# Other options:
+#   CHSH       - 'no' means the installer will not change the default shell (default: yes)
+#   RUNZSH     - 'no' means the installer will not run zsh after the install (default: yes)
+#   KEEP_ZSHRC - 'yes' means the installer will not replace an existing .zshrc (default: no)
+#
+# You can also pass some arguments to the install script to set some these options:
+#   --skip-chsh: has the same behavior as setting CHSH to 'no'
+#   --unattended: sets both CHSH and RUNZSH to 'no'
+#   --keep-zshrc: sets KEEP_ZSHRC to 'yes'
+# For example:
+#   sh install.sh --unattended
+#
+set -e
+
+# Default settings
+ZSH=${ZSH:-~/.oh-my-zsh}
+REPO=${REPO:-ohmyzsh/ohmyzsh}
+REMOTE=${REMOTE:-https://github.com/${REPO}.git}
+BRANCH=${BRANCH:-master}
+
+# Other options
+CHSH=${CHSH:-yes}
+RUNZSH=${RUNZSH:-yes}
+KEEP_ZSHRC=${KEEP_ZSHRC:-no}
+
+
+command_exists() {
+	command -v "$@" >/dev/null 2>&1
+}
+
+error() {
+	echo ${RED}"错误: $@"${RESET} >&2
+}
+
+setup_color() {
+	# Only use colors if connected to a terminal
+	if [ -t 1 ]; then
+		RED=$(printf '\033[31m')
+		GREEN=$(printf '\033[32m')
+		YELLOW=$(printf '\033[33m')
+		BLUE=$(printf '\033[34m')
+		BOLD=$(printf '\033[1m')
+		RESET=$(printf '\033[m')
+	else
+		RED=""
+		GREEN=""
+		YELLOW=""
+		BLUE=""
+		BOLD=""
+		RESET=""
+	fi
+}
+
+setup_ohmyzsh() {
+	# Prevent the cloned repository from having insecure permissions. Failing to do
+	# so causes compinit() calls to fail with "command not found: compdef" errors
+	# for users with insecure umasks (e.g., "002", allowing group writability). Note
+	# that this will be ignored under Cygwin by default, as Windows ACLs take
+	# precedence over umasks except for filesystems mounted with option "noacl".
+	umask g-w,o-w
+
+	echo "${BLUE}正在克隆Oh My Zsh...${RESET}"
+
+	command_exists git || {
+		error "git未安装"
+		exit 1
+	}
+
+	if [ "$OSTYPE" = cygwin ] && git --version | grep -q msysgit; then
+		error "Windows/MSYS Git不支持Cygwin"
+		error "确保Cygwin git软件包 已安装并且在\$PATH中。"
+		exit 1
+	fi
+
+	git clone -c core.eol=lf -c core.autocrlf=false \
+		-c fsck.zeroPaddedFilemode=ignore \
+		-c fetch.fsck.zeroPaddedFilemode=ignore \
+		-c receive.fsck.zeroPaddedFilemode=ignore \
+		--depth=1 --branch "$BRANCH" "$REMOTE" "$ZSH" || {
+		error "克隆oh-my-zsh仓库失败"
+		exit 1
+	}
+
+	echo
+}
+
+setup_zshrc() {
+	# Keep most recent old .zshrc at .zshrc.pre-oh-my-zsh, and older ones
+	# with datestamp of installation that moved them aside, so we never actually
+	# destroy a user's original zshrc
+	echo "${BLUE}正在查找已存在的zsh配置...${RESET}"
+
+	# Must use this exact name so uninstall.sh can find it
+	OLD_ZSHRC=~/.zshrc.pre-oh-my-zsh
+	if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]; then
+		# Skip this if the user doesn't want to replace an existing .zshrc
+		if [ $KEEP_ZSHRC = yes ]; then
+			echo "${YELLOW}发现~/.zshrc.${RESET} ${GREEN}保存中...${RESET}"
+			return
+		fi
+		if [ -e "$OLD_ZSHRC" ]; then
+			OLD_OLD_ZSHRC="${OLD_ZSHRC}-$(date +%Y-%m-%d_%H-%M-%S)"
+			if [ -e "$OLD_OLD_ZSHRC" ]; then
+				error "${OLD_OLD_ZSHRC}已经存在，无法备份${OLD_ZSHRC}"
+				error "请于一会儿后重新运行安装程序"
+				exit 1
+			fi
+			mv "$OLD_ZSHRC" "${OLD_OLD_ZSHRC}"
+
+			echo "${YELLOW}发现旧版于~/.zshrc.pre-oh-my-zsh." \
+				"${GREEN}正在备份至${OLD_OLD_ZSHRC}${RESET}"
+		fi
+		echo "${YELLOW}发现~/.zshrc${RESET}，${GREEN}正在备份至${OLD_ZSHRC}${RESET}"
+		mv ~/.zshrc "$OLD_ZSHRC"
+	fi
+
+	echo "${GREEN}正在使用Oh My Zsh模板文件并将其添加至~/.zshrc.${RESET}"
+
+	sed "/^export ZSH=/ c\\
+export ZSH=\"$ZSH\"
+" "$ZSH/templates/zshrc.zsh-template" > ~/.zshrc-omztemp
+	mv -f ~/.zshrc-omztemp ~/.zshrc
+
+	echo
+}
+
+setup_shell() {
+	# Skip setup if the user wants or stdin is closed (not running interactively).
+	if [ $CHSH = no ]; then
+		return
+	fi
+
+	# If this user's login shell is already "zsh", do not attempt to switch.
+	if [ "$(basename "$SHELL")" = "zsh" ]; then
+		return
+	fi
+
+	# If this platform doesn't provide a "chsh" command, bail out.
+	if ! command_exists chsh; then
+		cat <<-EOF
+			 我们无法自动切换您的shell，因为系统中不存在chsh。
+			${BLUE}请尝试手动更改默认为zsh${RESET}
+		EOF
+		return
+	fi
+
+	echo "${BLUE}是时候将您的默认shell更改为zsh:${RESET}"
+
+	# Prompt for user choice on changing the default login shell
+	printf "${YELLOW}您想将默认shell切换为zsh吗？ [Y/n]${RESET} "
+	read opt
+	case $opt in
+		y*|Y*|"") echo "正在更改shell..." ;;
+		n*|N*) echo "跳过shell的更改环节"; return ;;
+		*) echo "Invalid choice. Shell change skipped."; return ;;
+	esac
+
+	# Check if we're running on Termux
+	case "$PREFIX" in
+		*com.termux*) termux=true; zsh=zsh ;;
+		*) termux=false ;;
+	esac
+
+	if [ "$termux" != true ]; then
+		# Test for the right location of the "shells" file
+		if [ -f /etc/shells ]; then
+			shells_file=/etc/shells
+		elif [ -f /usr/share/defaults/etc/shells ]; then # Solus OS
+			shells_file=/usr/share/defaults/etc/shells
+		else
+			error "未发现文件/etc/shells，请尝试手动更改默认shell。"
+			return
+		fi
+
+		# Get the path to the right zsh binary
+		# 1. Use the most preceding one based on $PATH, then check that it's in the shells file
+		# 2. If that fails, get a zsh path from the shells file, then check it actually exists
+		if ! zsh=$(which zsh) || ! grep -qx "$zsh" "$shells_file"; then
+			if ! zsh=$(grep '^/.*/zsh$' "$shells_file" | tail -1) || [ ! -f "$zsh" ]; then
+				error "没有发现zsh的可执行二进制文件，或者'$shells_file'并不存在。"
+				error "请手动更改默认shell"
+				return
+			fi
+		fi
+	fi
+
+	# We're going to change the default shell, so back up the current one
+	if [ -n "$SHELL" ]; then
+		echo $SHELL > ~/.shell.pre-oh-my-zsh
+	else
+		grep "^$USER:" /etc/passwd | awk -F: '{print $7}' > ~/.shell.pre-oh-my-zsh
+	fi
+
+	# Actually change the default shell to zsh
+	if ! chsh -s "$zsh"; then
+		error "chsh命令运行失败，请尝试手动更改默认shell。"
+	else
+		export SHELL="$zsh"
+		echo "${GREEN}Shell成功被更改为'$zsh'.${RESET}"
+	fi
+
+	echo
+}
+
+main() {
+	# Run as unattended if stdin is closed
+	if [ ! -t 0 ]; then
+		RUNZSH=no
+		CHSH=no
+	fi
+
+	# Parse arguments
+	while [ $# -gt 0 ]; do
+		case $1 in
+			--unattended) RUNZSH=no; CHSH=no ;;
+			--skip-chsh) CHSH=no ;;
+			--keep-zshrc) KEEP_ZSHRC=yes ;;
+		esac
+		shift
+	done
+
+	setup_color
+
+	if ! command_exists zsh; then
+		echo "${YELLOW}还未被安装。${RESET} 请先安装zsh！"
+		exit 1
+	fi
+
+	if [ -d "$ZSH" ]; then
+		cat <<-EOF
+			${YELLOW}You already have Oh My Zsh installed.${RESET}
+			 您将需要移除'$ZSH' 若您想重新安装。
+		EOF
+		exit 1
+	fi
+
+	setup_ohmyzsh
+	setup_zshrc
+	setup_shell
+
+	printf "$GREEN"
+	cat <<-'EOF'
+		         __                                     __
+		  ____  / /_     ____ ___  __  __   ____  _____/ /_
+		 / __ \/ __ \   / __ `__ \/ / / /  /_  / / ___/ __ \
+		/ /_/ / / / /  / / / / / / /_/ /    / /_(__  ) / / /
+		\____/_/ /_/  /_/ /_/ /_/\__, /    /___/____/_/ /_/
+		                        /____/                       ....安装完成！
+
+		在你为Oh My Zsh!尖叫之前，请检查 ~/.zshrc 文件以选择插件，主题, 以及进行控制。
+
+		• 官方推特: https://twitter.com/ohmyzsh
+		• Discord server: https://discord.gg/ohmyzsh
+		• 官方周边: https://shop.planetargon.com/collections/oh-my-zsh
+
+	EOF
+	printf "$RESET"
+
+	if [ $RUNZSH = no ]; then
+		echo "${YELLOW}Run zsh to try it out.${RESET}"
+		exit
+	fi
+
+	exec zsh -l
+}
+
+main "$@"
